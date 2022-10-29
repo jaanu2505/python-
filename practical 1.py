a82f271ca8885125f21761abfc219f22c0a3d4be
jaanu2505 <116936179+jaanu2505@users.noreply.github.com>
2022-10-29 11:06:13 +0530

    Create practical 1.py

---
+++ practical 1.py
@@ -0,0 +1,8 @@
+letter = input ("input a letter of an alphabet:")
+if letter in ('a','e','i','o','u'):
+   print ("it is a vowel ")
+elif letter == 'y':
+   print ("sometimes letter y is stand for vowel , sometimes stand for constant ")
+else :
+   print ("it is a constant ")
+   
