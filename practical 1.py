a1a48ae41ab8acf3e46b8d373c366619570fc6c6
jaanu2505 <116936179+jaanu2505@users.noreply.github.com>
2023-12-25 11:46:05 +0530

    practical 1.py

--- practical 1.py
+++ practical 1.py
@@ -2,7 +2,7 @@ letter = input ("input a letter of an alphabet:")
 if letter in ('a','e','i','o','u'):
    print ("it is a vowel ")
 elif letter == 'y':
-   print ("sometimes letter y is stand for vowel , sometimes stand for constant ")
+   print ("sometimes letter y stands for vowel, sometimes it stands for constant ")
 else :
    print ("it is a constant ")
    
